d1c8554f75d7dce4817f8881ef23138968256b5b
caleb84loarca <47404154+caleb84loarca@users.noreply.github.com>
2022-01-22 22:51:28 -0600

    Archivos
    Agregando archivos al repositorio

---
+++ scrip_pagina.js
@@ -0,0 +1,27 @@
+//variables HTML
+
+var txtinput = document.getElementById('txtFrase');
+
+//var txtinput = document.querySelector('txtFrase');
+
+var TextoEncriptado = document.getElementById('txtEncriptado');
+
+var botonEncriptar = document.getElementById('BtnEncriptar');
+var botonDesencriptar = document.getElementById('BtnDesencriptar');
+
+var frase = txtinput;
+
+
+// funciones
+
+function obtenertexto() {
+     frase;
+     
+     return this.frase;
+   }
+
+botonEncriptar.addEventListener('click', frase);
+
+    
+
+
